f86b172a743cb6ce011283d74b66dddc24cd312d
Solype <ambroise.jacquemet-ramirez-vega@epitech.eu>
2024-11-18 17:24:21 +0100

    [MOD] CMakeLists.txt -> modified the Cmake to check the current plateform compiled on

--- main.cpp
+++ main.cpp
@@ -7,8 +7,14 @@
 
 #include <iostream>
 
-int main(void)
-{
-    std::cout << "Hello, World!" << std::endl;
+int main() {
+    // Vérifier la plateforme
+    #ifdef PLATFORM_WINDOWS
+        std::cout << "Running on Windows platform." << std::endl;
+    #elif defined(PLATFORM_LINUX)
+        std::cout << "Running on Linux platform." << std::endl;
+    #else
+        std::cout << "Unknown platform." << std::endl;
+    #endif
     return 0;
 }
