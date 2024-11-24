223654c36b9eb2cb40cb2e22fe0f5dacdad6024c
Solype <ambroise.jacquemet-ramirez-vega@epitech.eu>
2024-11-24 18:19:49 +0100

    [ADD] test on main.cpp

--- ecs/main.cpp
+++ ecs/main.cpp
@@ -2,12 +2,25 @@
 #include <iostream>
 #include <string>
 
+struct a_s {
+    int a;
+    std::string b;
+    a_s(int a, std::string const &b) : a(a), b(b) {}
+    ~a_s() {}
+};
+
 int main() {
     registry reg;
     entity e = reg.create_entity();
     reg.register_component<int>();
     reg.emplace_component<int>(e, 42);
 
+    reg.register_component<std::string>();
+    reg.emplace_component<std::string>(e, "Hello, World!");
+
+    reg.register_component<a_s>();
+    reg.emplace_component<a_s>(e, 42, "Hello, World!");
+    reg.delete_entity(e);
 
     return 0;
 }
