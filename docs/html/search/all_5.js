a1ba1e32d304087800e01e3eced7e850e1889abf
Solype <ambroise.jacquemet-ramirez-vega@epitech.eu>
2024-11-25 16:27:14 +0100

    [ADD] some documentation in entity.hpp and isystem.hpp

--- docs/html/search/all_5.js
+++ docs/html/search/all_5.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['insert_5fat_0',['insert_at',['../classsparse__array.html#a148de23c16b154614541d8c3ba6d4686',1,'sparse_array::insert_at(size_type pos, Component const &amp;)'],['../classsparse__array.html#a34bcc8149a3c76ebe297201d0905fae0',1,'sparse_array::insert_at(size_type pos, Component &amp;&amp;)']]],
-  ['isystem_1',['isystem',['../classecs_1_1isystem.html',1,'ecs']]],
+  ['isystem_1',['isystem',['../classecs_1_1isystem.html',1,'ecs::isystem&lt; Components &gt;'],['../classecs_1_1isystem.html#acf994aa668e56219129cb03d59936dcc',1,'ecs::isystem::isystem()']]],
   ['isystem_3c_20position_2c_20velocity_20_3e_2',['isystem&lt; position, velocity &gt;',['../classecs_1_1isystem.html',1,'ecs']]],
   ['iterator_3',['iterator',['../classsparse__array.html#a1293c5b64d77bb97ed87e57c6276925e',1,'sparse_array']]]
 ];
