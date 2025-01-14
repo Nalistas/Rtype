01121a53e9ab622421dad7b5898921c080dcc075
hugo tesson <tesson.hugo@orange.fr>
2025-01-14 17:20:12 +0100

    [ADD] documentation for the server include

--- rework_project/server/include/TcpProtocol.hpp
+++ rework_project/server/include/TcpProtocol.hpp
@@ -18,23 +18,91 @@
 
 class TcpProtocol {
     public:
+
+        /**
+         * @brief Construct a new TcpProtocol object
+         * 
+         * @param rooms Reference to a map of room IDs and Room objects
+         * @param clients Reference to a map of client sockets and Client objects
+         * @param tcpServer Reference to a TcpServer object
+         */
         TcpProtocol(
             std::map<uint8_t, Room> &rooms,
             std::map<std::shared_ptr<asio::ip::tcp::socket>, Client> &clients,
             TcpServer &tcpServer
         );
+
+        /**
+         * @brief Destroy the TcpProtocol object
+         * 
+         */
         ~TcpProtocol();
 
+        /**
+         * @brief Treat a client command
+         * 
+         * @param client The client socket
+         * @param params The command parameters
+         * @return int 0 if success, -1 if error
+         * 
+         */
         int interpreter(std::shared_ptr<asio::ip::tcp::socket> &client, std::vector<uint8_t> data);
+
+        /**
+         * @brief List the rooms for a client
+         * 
+         * @param client The client socket
+         */
         void listRooms(std::shared_ptr<asio::ip::tcp::socket> &client);
+
+        /**
+         * @brief Enter a room for a client
+         * 
+         * @param client The client socket
+         * @param roomId The room ID
+         */
         void enterRoom(std::shared_ptr<asio::ip::tcp::socket> &client, uint8_t roomId);
+
+        /**
+         * @brief Create a room for a client
+         * 
+         * @param client The client socket
+         * @param roomName The room name
+         * @param gameName The game name
+         */
         void createRoom(std::shared_ptr<asio::ip::tcp::socket> &client, std::string roomName, std::string gameName);
+
+        /**
+         * @brief Exit a room for a client
+         * 
+         * @param client The client socket
+         */
         void exitRoom(std::shared_ptr<asio::ip::tcp::socket> &client);
+
+        /**
+         * @brief Rename a room for a client
+         * 
+         * @param client The client socket
+         * @param roomId The room ID
+         * @param roomName The room name
+         */
         void renameRoom(std::shared_ptr<asio::ip::tcp::socket> &client, uint8_t roomId, std::string roomName);
+
+        /**
+         * @brief Delete a room for a client
+         * 
+         * @param client The client socket
+         * @param roomId The room ID
+         */
         void deleteRoom(std::shared_ptr<asio::ip::tcp::socket> &client, uint8_t roomId);
+
+        /**
+         * @brief transform an image to binary
+         * 
+         * @param path the path of the image
+         * @return std::vector<uint8_t> the binary image
+         */
         std::vector<uint8_t> image_to_binary(const std::string &path);
-        void sendImage(std::shared_ptr<asio::ip::tcp::socket> &client, uint8_t spriteId, uint32_t sizeX, uint32_t sizeY, uint32_t width, uint32_t height,
-    uint32_t offsetX, uint32_t offsetY, uint8_t nbFrames, uint32_t frameDelay, const std::string &path);
 
     private:
         std::unordered_map<uint8_t, std::function<void(std::shared_ptr<asio::ip::tcp::socket> &, std::istringstream&)>> _commandMap;
