1d2adb1d0b4e5790ad60ac4ccb931e967108c4c3
Nalistas <114913840+Nalistas@users.noreply.github.com>
2025-01-20 13:42:14 +0000

    Deploying to gh-pages from @ Nalistas/Rtype@9108f2db7ef174a6939c7c2bd2587489d263e093 🚀

--- docs/search/functions_2.js
+++ docs/search/functions_2.js
@@ -1,12 +1,13 @@
 var searchData=
 [
   ['changestatus_0',['changeStatus',['../classTcpProtocol.html#ab9d3fc9c2c37ee916030199d3bb0a9f3',1,'TcpProtocol']]],
-  ['clear_1',['clear',['../classraylib_1_1Window.html#a5e0a87fa3605ba71d4fbc85ef096dd1e',1,'raylib::Window']]],
-  ['clientactionlog_2',['ClientActionLog',['../classClientActionLog.html#ab4edc4d103f21c39e57878da708522ac',1,'ClientActionLog']]],
-  ['clientroom_3',['ClientRoom',['../classClientRoom.html#a101ced9191e87be9998f8b65af1c1b22',1,'ClientRoom']]],
-  ['close_4',['close',['../classraylib_1_1Window.html#a1a7d0bb59ec6227cf22ba59c46bd0cbc',1,'raylib::Window::close()'],['../classDLLdr_1_1DLLoader.html#ab2fb5201284e43745b0e246f38504f96',1,'DLLdr::DLLoader::close()']]],
-  ['core_5',['Core',['../classCore.html#a0a12cb32035ca501a04660d33858f5c5',1,'Core']]],
-  ['create_5fentity_6',['create_entity',['../classecs_1_1registry.html#a3dd0770aa04c88c98c260b64daee967c',1,'ecs::registry']]],
-  ['createplayer_7',['createplayer',['../classRtype.html#a97cb139753d4da09be83c2958a76c3a1',1,'Rtype::createPlayer()'],['../classRType.html#a6ff2753581ab28463edbd84a150c9ef6',1,'RType::createPlayer()'],['../classrtype_1_1IGame.html#ae2d0395a6beb5f2980fb8f2c1be86261',1,'rtype::IGame::createPlayer()']]],
-  ['createroom_8',['createRoom',['../classTcpProtocol.html#a0e348023142f975cc6f23b94a7aef29b',1,'TcpProtocol']]]
+  ['checkcollision_1',['checkCollision',['../classSystemCollision.html#a06dfa10b99397d02041b15f37759fcbe',1,'SystemCollision']]],
+  ['clear_2',['clear',['../classraylib_1_1Window.html#a5e0a87fa3605ba71d4fbc85ef096dd1e',1,'raylib::Window']]],
+  ['clientactionlog_3',['ClientActionLog',['../classClientActionLog.html#ab4edc4d103f21c39e57878da708522ac',1,'ClientActionLog']]],
+  ['clientroom_4',['ClientRoom',['../classClientRoom.html#a101ced9191e87be9998f8b65af1c1b22',1,'ClientRoom']]],
+  ['close_5',['close',['../classraylib_1_1Window.html#a1a7d0bb59ec6227cf22ba59c46bd0cbc',1,'raylib::Window::close()'],['../classDLLdr_1_1DLLoader.html#ab2fb5201284e43745b0e246f38504f96',1,'DLLdr::DLLoader::close()']]],
+  ['core_6',['Core',['../classCore.html#a0a12cb32035ca501a04660d33858f5c5',1,'Core']]],
+  ['create_5fentity_7',['create_entity',['../classecs_1_1registry.html#a3dd0770aa04c88c98c260b64daee967c',1,'ecs::registry']]],
+  ['createplayer_8',['createplayer',['../classRtype.html#a97cb139753d4da09be83c2958a76c3a1',1,'Rtype::createPlayer()'],['../classRType.html#a6ff2753581ab28463edbd84a150c9ef6',1,'RType::createPlayer()'],['../classrtype_1_1IGame.html#ae2d0395a6beb5f2980fb8f2c1be86261',1,'rtype::IGame::createPlayer()']]],
+  ['createroom_9',['createRoom',['../classTcpProtocol.html#a0e348023142f975cc6f23b94a7aef29b',1,'TcpProtocol']]]
 ];
--- docs/search/functions_a.js
+++ docs/search/functions_a.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['launch_0',['launch',['../classGameLauncher.html#a7b6d009010b33015156599c28c58c01e',1,'GameLauncher']]],
-  ['listgames_1',['listGames',['../classTcpProtocol.html#a1afc3adbd42e7b943f6c4645c37f5fa0',1,'TcpProtocol']]],
-  ['listrooms_2',['listRooms',['../classTcpProtocol.html#a3e7cf7d70064d6b81cf4cba05989054a',1,'TcpProtocol']]],
-  ['loop_3',['loop',['../classBackground.html#abdf37da3904aef7a47608118e48009e0',1,'Background']]]
+  ['lefthandlers_1',['LeftHandlers',['../classLeftHandlers.html#aa75fab707f57b9e6c9a3c86467d0dd2b',1,'LeftHandlers']]],
+  ['listgames_2',['listGames',['../classTcpProtocol.html#a1afc3adbd42e7b943f6c4645c37f5fa0',1,'TcpProtocol']]],
+  ['listrooms_3',['listRooms',['../classTcpProtocol.html#a3e7cf7d70064d6b81cf4cba05989054a',1,'TcpProtocol']]],
+  ['loop_4',['loop',['../classBackground.html#abdf37da3904aef7a47608118e48009e0',1,'Background']]]
 ];
