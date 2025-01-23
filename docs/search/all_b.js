674ad05fd64ba940d47229c7cf2655be7dc2242d
ugeaux <81755663+ugeaux@users.noreply.github.com>
2025-01-23 10:21:52 +0000

    Deploying to gh-pages from @ Nalistas/Rtype@148c5bdeb90887adeefe44685dc09758deb81d80 🚀

--- docs/search/all_b.js
+++ docs/search/all_b.js
@@ -3,12 +3,13 @@ var searchData=
   ['features_0',['Features',['../index.html#autotoc_md32',1,'']]],
   ['for_20linux_20users_3a_1',['For Linux users:',['../index.html#autotoc_md38',1,'']]],
   ['for_20windows_20you_20need_20to_20have_3a_2',['For Windows, you need to have:',['../index.html#autotoc_md36',1,'']]],
-  ['fork_3',['fork',['../classprocess_1_1Process.html#abf5f439dc2a978e842c45608d836ffbc',1,'process::Process']]],
-  ['formatforceregisterinroom_4',['formatForceRegisterInRoom',['../classTcpProtocol.html#ab01c8286584e5a67ff1b45a68de6d42d',1,'TcpProtocol']]],
-  ['formatgameavailable_5',['formatGameAvailable',['../classTcpProtocol.html#ad9966863e9a6409de65432a8dad5ed4e',1,'TcpProtocol']]],
-  ['formatko_6',['formatKo',['../classTcpProtocol.html#a8ed22951210721c37090b1fb4975be7e',1,'TcpProtocol']]],
-  ['formatlaunchroom_7',['formatLaunchRoom',['../classTcpProtocol.html#a0960445699328e3fc986b0e45ec18b2a',1,'TcpProtocol']]],
-  ['formatok_8',['formatOk',['../classTcpProtocol.html#af614707d08ef8acd3770f511f66c7408',1,'TcpProtocol']]],
-  ['formatplayerroomupdate_9',['formatPlayerRoomUpdate',['../classTcpProtocol.html#a6aac3e57e5e00e298bdcde2435639fae',1,'TcpProtocol']]],
-  ['formatroomcreateddeleted_10',['formatRoomCreatedDeleted',['../classTcpProtocol.html#a1e2336bb7b036085e43d71fb8b2d021e',1,'TcpProtocol']]]
+  ['forceinroom_3',['forceInRoom',['../classCore.html#a38f6f5c08428359810ff87af7c06cdf7',1,'Core']]],
+  ['fork_4',['fork',['../classprocess_1_1Process.html#abf5f439dc2a978e842c45608d836ffbc',1,'process::Process']]],
+  ['formatforceregisterinroom_5',['formatForceRegisterInRoom',['../classTcpProtocol.html#ab01c8286584e5a67ff1b45a68de6d42d',1,'TcpProtocol']]],
+  ['formatgameavailable_6',['formatGameAvailable',['../classTcpProtocol.html#ad9966863e9a6409de65432a8dad5ed4e',1,'TcpProtocol']]],
+  ['formatko_7',['formatKo',['../classTcpProtocol.html#a8ed22951210721c37090b1fb4975be7e',1,'TcpProtocol']]],
+  ['formatlaunchroom_8',['formatLaunchRoom',['../classTcpProtocol.html#a0960445699328e3fc986b0e45ec18b2a',1,'TcpProtocol']]],
+  ['formatok_9',['formatOk',['../classTcpProtocol.html#af614707d08ef8acd3770f511f66c7408',1,'TcpProtocol']]],
+  ['formatplayerroomupdate_10',['formatPlayerRoomUpdate',['../classTcpProtocol.html#a6aac3e57e5e00e298bdcde2435639fae',1,'TcpProtocol']]],
+  ['formatroomcreateddeleted_11',['formatRoomCreatedDeleted',['../classTcpProtocol.html#a1e2336bb7b036085e43d71fb8b2d021e',1,'TcpProtocol']]]
 ];
--- docs/search/functions_5.js
+++ docs/search/functions_5.js
@@ -1,11 +1,12 @@
 var searchData=
 [
-  ['fork_0',['fork',['../classprocess_1_1Process.html#abf5f439dc2a978e842c45608d836ffbc',1,'process::Process']]],
-  ['formatforceregisterinroom_1',['formatForceRegisterInRoom',['../classTcpProtocol.html#ab01c8286584e5a67ff1b45a68de6d42d',1,'TcpProtocol']]],
-  ['formatgameavailable_2',['formatGameAvailable',['../classTcpProtocol.html#ad9966863e9a6409de65432a8dad5ed4e',1,'TcpProtocol']]],
-  ['formatko_3',['formatKo',['../classTcpProtocol.html#a8ed22951210721c37090b1fb4975be7e',1,'TcpProtocol']]],
-  ['formatlaunchroom_4',['formatLaunchRoom',['../classTcpProtocol.html#a0960445699328e3fc986b0e45ec18b2a',1,'TcpProtocol']]],
-  ['formatok_5',['formatOk',['../classTcpProtocol.html#af614707d08ef8acd3770f511f66c7408',1,'TcpProtocol']]],
-  ['formatplayerroomupdate_6',['formatPlayerRoomUpdate',['../classTcpProtocol.html#a6aac3e57e5e00e298bdcde2435639fae',1,'TcpProtocol']]],
-  ['formatroomcreateddeleted_7',['formatRoomCreatedDeleted',['../classTcpProtocol.html#a1e2336bb7b036085e43d71fb8b2d021e',1,'TcpProtocol']]]
+  ['forceinroom_0',['forceInRoom',['../classCore.html#a38f6f5c08428359810ff87af7c06cdf7',1,'Core']]],
+  ['fork_1',['fork',['../classprocess_1_1Process.html#abf5f439dc2a978e842c45608d836ffbc',1,'process::Process']]],
+  ['formatforceregisterinroom_2',['formatForceRegisterInRoom',['../classTcpProtocol.html#ab01c8286584e5a67ff1b45a68de6d42d',1,'TcpProtocol']]],
+  ['formatgameavailable_3',['formatGameAvailable',['../classTcpProtocol.html#ad9966863e9a6409de65432a8dad5ed4e',1,'TcpProtocol']]],
+  ['formatko_4',['formatKo',['../classTcpProtocol.html#a8ed22951210721c37090b1fb4975be7e',1,'TcpProtocol']]],
+  ['formatlaunchroom_5',['formatLaunchRoom',['../classTcpProtocol.html#a0960445699328e3fc986b0e45ec18b2a',1,'TcpProtocol']]],
+  ['formatok_6',['formatOk',['../classTcpProtocol.html#af614707d08ef8acd3770f511f66c7408',1,'TcpProtocol']]],
+  ['formatplayerroomupdate_7',['formatPlayerRoomUpdate',['../classTcpProtocol.html#a6aac3e57e5e00e298bdcde2435639fae',1,'TcpProtocol']]],
+  ['formatroomcreateddeleted_8',['formatRoomCreatedDeleted',['../classTcpProtocol.html#a1e2336bb7b036085e43d71fb8b2d021e',1,'TcpProtocol']]]
 ];
