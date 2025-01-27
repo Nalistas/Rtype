c630fff1ebb4a6f6e3213ba921490485d2d4fac2
Nalistas <Nalistas@users.noreply.github.com>
2025-01-27 14:53:48 +0000

    Deploying to gh-pages from @ Nalistas/Rtype@a79c4adb42f2f752b5efe318686a362a5a1a28b2 🚀

--- docs/search/all_8.js
+++ docs/search/all_8.js
@@ -1,10 +1,8 @@
 var searchData=
 [
-  ['handler1_0',['handler1',['../classhandler1.html',1,'']]],
-  ['handler2_1',['handler2',['../classhandler2.html',1,'']]],
-  ['hasdata_2',['hasdata',['../classTcpClient.html#aa3d14d1c5c6e51c854b6fa535b89be5f',1,'TcpClient::hasData()'],['../classUdpClient.html#ad5e69157113e0e5b4e2302273484ce31',1,'UdpClient::hasData()']]],
-  ['hasdatatoread_3',['hasdatatoread',['../classTcpServer.html#a352445711975fc138790e8b05ba62c9a',1,'TcpServer::hasDataToRead()'],['../classUdpServer.html#a84aa6a3f9b4726fbec61e3760b5292d4',1,'UdpServer::hasDataToRead()']]],
-  ['have_3a_4',['For Windows, you need to have:',['../index.html#autotoc_md18',1,'']]],
-  ['hitbox_5',['Hitbox',['../structHitbox.html',1,'']]],
-  ['how_20to_20play_6',['How to Play',['../index.html#autotoc_md22',1,'']]]
+  ['hasdata_0',['hasdata',['../classTcpClient.html#aa3d14d1c5c6e51c854b6fa535b89be5f',1,'TcpClient::hasData()'],['../classUdpClient.html#ad5e69157113e0e5b4e2302273484ce31',1,'UdpClient::hasData()']]],
+  ['hasdatatoread_1',['hasdatatoread',['../classTcpServer.html#a352445711975fc138790e8b05ba62c9a',1,'TcpServer::hasDataToRead()'],['../classUdpServer.html#a84aa6a3f9b4726fbec61e3760b5292d4',1,'UdpServer::hasDataToRead()']]],
+  ['have_3a_2',['For Windows, you need to have:',['../index.html#autotoc_md18',1,'']]],
+  ['hitbox_3',['Hitbox',['../structHitbox.html',1,'']]],
+  ['how_20to_20play_4',['How to Play',['../index.html#autotoc_md22',1,'']]]
 ];
--- docs/search/classes_6.js
+++ docs/search/classes_6.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['handler1_0',['handler1',['../classhandler1.html',1,'']]],
-  ['handler2_1',['handler2',['../classhandler2.html',1,'']]],
-  ['hitbox_2',['Hitbox',['../structHitbox.html',1,'']]]
+  ['hitbox_0',['Hitbox',['../structHitbox.html',1,'']]]
 ];
